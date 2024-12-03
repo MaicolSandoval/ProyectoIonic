9da6c0ddeba801c48c68e970345745626677ec0a
Maicol Sandoval <maicolsandoval55@gmail.com>
2024-12-03 00:19:54 -0500

    Validaciones de ingreso

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonIcon, IonButton, IonItem, IonList, IonCheckbox, IonLabel, IonGrid, IonRow, IonCol  } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonIcon, IonButton, IonItem, IonList, IonCheckbox, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -12,10 +12,29 @@ import { RouterLink } from '@angular/router';
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonIcon, IonButton, IonCheckbox, IonLabel, IonGrid, IonRow, IonCol, CommonModule, FormsModule, RouterLink]
 })
 export class MainPage implements OnInit {
+  correo = false;
+  contrasena = false;
+  campo1Error: boolean = false;
+  campo2Error: boolean = false;
+  constructor(private router: Router){};
 
-  constructor() { }
-
+  //onBlurCampo1() { this.campo1Error = !this.campo1; // Si el campo está vacío, campo1Error es true
+  
+  onBlurCorreo(campo: IonInput) {
+    const valor = campo.value != "" ? true : false;
+    this.correo = valor;
+  }
+  onBlurContrasena(campo: IonInput) {
+    const valor = campo.value != "" ? true : false; 
+    this.contrasena = valor
+  }
+  navegar() { 
+    if (this.correo && this.contrasena) 
+      { this.router.navigate(['/page-tareas']); } 
+    else{
+      alert("los campos no esta debidamente diligenciados")
+    }
+  }
   ngOnInit() {
   }
-
 }
--- src/app/page-register/page-register.page.ts
+++ src/app/page-register/page-register.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonIcon, IonButton, IonItem, IonList, IonCheckbox, IonLabel } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-register',
@@ -12,8 +12,29 @@ import { RouterLink } from '@angular/router';
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonIcon, IonButton, IonCheckbox, IonLabel, CommonModule, FormsModule, RouterLink]
 })
 export class PageRegisterPage implements OnInit {
+  correo = false;
+  contrasena = false;
+  campo1Error: boolean = false;
+  campo2Error: boolean = false;
+  constructor(private router: Router){};
 
-  constructor() { }
+  //onBlurCampo1() { this.campo1Error = !this.campo1; // Si el campo está vacío, campo1Error es true
+  
+  onBlurCorreo(campo: IonInput) {
+    const valor = campo.value != "" ? true : false;
+    this.correo = valor;
+  }
+  onBlurContrasena(campo: IonInput) {
+    const valor = campo.value != "" ? true : false; 
+    this.contrasena = valor
+  }
+  navegar() { 
+    if (this.correo && this.contrasena) 
+      { this.router.navigate(['/page-tareas']); } 
+    else{
+      alert("los campos no esta debidamente diligenciados")
+    }
+  }
 
   ngOnInit() {
   }
